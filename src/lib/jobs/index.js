039a677c65e95925dc9e5ac004309aeaec7a0d6c
kevintoor <kevin.toor@mimik.com>
2020-12-03 00:21:03 +0000

    EPOC-350 Removed unused code

--- src/lib/jobs/index.js
+++ src/lib/jobs/index.js
@@ -4,8 +4,6 @@ const gatewayNodeSyncJob = require('./gatewayNodeSyncJob');
 const startJobs = () => gatewayNodeSyncJob.start()
   .then(() => edgeNodeSyncJob.start());
 
-// const startJobs = () => edgeNodeSyncJob.start();
-
 module.exports = {
   startJobs,
 };
